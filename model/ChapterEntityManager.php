02459f6c7fc74dc9003467eddbaee0d181d35c6f
arthur32810 <arthur32810@hotmail.fr>
2017-12-14 18:34:13 +0100

    Création de la fonction getAllChapter

--- model/ChapterEntityManager.php
+++ model/ChapterEntityManager.php
@@ -1,1 +1,10 @@
 <?php
+	require 'DBManager.php';
+
+	class ChapterEntityManager{
+		public static function getAllChapter(){
+			$db = DBManager::dbConnect();
+			$req = $db->query('SELECT * FROM chapter');
+			return $req;
+		}
+	}
