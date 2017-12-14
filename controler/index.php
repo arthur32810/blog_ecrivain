187e997651dd92ea4aa97963cab1decdefe0436a
arthur32810 <arthur32810@hotmail.fr>
2017-12-14 18:59:56 +0100

    construction de l'action

--- controler/index.php
+++ controler/index.php
@@ -7,5 +7,13 @@ require 'Controler.php';
 			$chapters = Chapter::allChapter();
 			require '../view/ChaptersView.php';
 		}
-		elseif()
+		elseif ($_GET['action'] == 'post'){ // Demande le chapitre $_GET['id'];
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				$id = $_GET['id'];
+				$chapter = Chapter::Chapter($id);
+			}
+			else {
+				header('Location: index.php?action=allChapter&existPost=no');
+			}
+		}
 	}
