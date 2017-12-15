68a5d28dcb9ce0bafafca06b192185f5ce5e1cd7
arthur32810 <arthur32810@hotmail.fr>
2017-12-15 12:40:12 +0100

    Création de la vue et du traitement de formulaire

---
+++ view/chapter.php
@@ -0,0 +1,131 @@
+<?php
+if(isset($_POST['add'])){
+	if(isset($_POST['chapter']) && isset($_POST['title']) && isset($_POST['content'])){
+		if(!empty(trim($_POST['chapter'])) && !empty(trim($_POST['title'])) && !empty(trim($_POST['content']))){
+			extract($_POST);
+
+			$chapter = htmlspecialchars($_POST['chapter']);
+			$title = htmlspecialchars($_POST['title']);
+			$content = htmlspecialchars($_POST['content']);
+
+			require_once '../model/ChapterEntity.php';
+			$chpater = new ChapterEntity(); // Instance de la classe PostEntity
+			$chapter->setChapter($chapter);
+			$chapter->setTitle($title);
+			$chapter->setContent($content);
+
+			require_once '../model/ChapterEntityManager.php';
+			$Chapter = ChapterEntityManager::getChapter($chapter); //Test si le chapitre existe
+
+			if(!empty($Chapter)){ //Chapitre existe, on redirige
+				echo '<meta http-equiv="refresh" content="0;URL=index.php?action=update_post&postId='.$existPost['id'].'&chapter=exist">';
+		    }
+
+		    else{ //il n'existe pas, on continue
+		    	$createPost = ChapterEntityManager::createPost($chapter);
+
+			    if ($createPost === false) {
+			    	echo '<meta http-equiv="refresh" content="0;URL=index.php?action=write_post&create=no">';
+			    }
+			    else {
+			    	echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&create=yes">';
+				}
+
+			}
+		}
+		else{
+			echo '<meta http-equiv="refresh" content="0;URL=index.php?action=write_post&complete=no">';
+		}
+	}
+	else{
+		echo '<meta http-equiv="refresh" content="0;URL=index.php?action=write_post&complete=no">';
+	}
+}
+
+elseif(isset($_POST['update'])){
+	if(isset($_POST['chapter']) && isset($_POST['title']) && isset($_POST['content'])){
+		if(!empty(trim($_POST['chapter'])) && !empty(trim($_POST['title'])) && !empty(trim($_POST['content']))){
+			extract($_POST);
+
+			$chapter = $_POST['chapter'];
+			$title = $_POST['title'];
+			$content = $_POST['content'];
+			$postId = $post['id'];
+
+			$post = new PostEntity();
+			$post->setId($postId);
+			$post->setChapter($chapter);
+			$post->setTitle($title);
+			$post->setContent($content);
+
+			$postManager = new Arthur\WriterBlog\Model\PostEntityManager();
+			$existPost = $postManager->getPost($post);
+
+			if(!empty($existPost)){ 
+				$updatePost = $postManager->updatePost($post);
+				if ($updatePost === false) {
+					echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&update=no">'; 
+				}
+				else {
+					echo '<meta http-equiv="refresh" content="0;URL=index.php?action=post&id='.$postId.'&update=yes">'; 
+				}
+
+			}
+			else{
+				echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&existPost=no">'; 
+			}
+		}
+		else{ 
+			echo '<meta http-equiv="refresh" content="0;URL=index.php?action=update_post&complete=no">'; 
+		}
+	}
+	else{ 
+		echo '<meta http-equiv="refresh" content="0;URL=index.php?action=update_post&complete=no">'; 
+	}
+}
+
+elseif(isset($_POST['delete'])){
+	if(isset($_POST['chapter']) && isset($_POST['title']) && isset($_POST['content'])){
+		if(!empty(trim($_POST['chapter'])) && !empty(trim($_POST['title'])) && !empty(trim($_POST['content']))){
+			extract($_POST);
+
+			$postId = $post['id'];
+
+			$post = new PostEntity();
+			$post->setId($postId);
+
+			$postManager = new Arthur\WriterBlog\Model\PostEntityManager();
+			$existPost = $postManager->getPost($post);
+
+			if(!empty($existPost)){ 
+
+				$deletePost = $postManager->deletePost($post);
+				if ($deletePost === false) {
+					echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&delete=no">';
+				}
+				else {
+					$commentManager = new Arthur\WriterBlog\Model\CommentEntityManager();
+
+					$deleteComment = $commentManager->deleteCommentChapter($post);
+					$deletePostModeration = $postManager->deletePostModeration($post);
+				    
+				    if ($deleteComment === false) {
+				    	echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&delete=no">';
+					}
+					else{
+						echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&delete=yes">';
+					}
+				}
+			}
+			else{
+				echo '<meta http-equiv="refresh" content="0;URL=index.php?action=listPosts&existPost=no">'; 
+			}
+		}
+		else{ 
+			echo '<meta http-equiv="refresh" content="0;URL=index.php?action=update_post&complete=no">'; 
+		}
+	}
+	else{ 
+		echo '<meta http-equiv="refresh" content="0;URL=index.php?action=update_post&complete=no">'; 
+	}
+}
---
+++ view/writeChapter.php
@@ -0,0 +1,51 @@
+<?php $title ='Création d\'un chapitre' ;
+	$script = ' <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=vhnlap27hf8ot2ip6dmzfw56ndq9anxv230bkh8i9oxmua2q"></script>
+  		<script>tinymce.init({ selector:\'textarea\' });</script>'?>
+
+<?php ob_start();
+		
+		if (!empty($_GET['create']) && $_GET['create'] == 'no'){
+			?><div class="alert alert-danger" role="alert">
+				Le chapitre n'a pas pu être ajouté
+			</div> <?php
+			}
+		elseif(!empty($_GET['chapter']) && $_GET['chapter'] == 'exist'){
+				?> <div class="alert alert-success" role="alert">
+				 	Le chapitre existe déjà !
+				</div> <?php
+		}
+		elseif (!empty($_GET['complete']) && $_GET['complete'] == 'no'){
+				?><div class="alert alert-danger" role="alert">
+					Les informations ne sont pas complétes
+				</div> <?php
+			}
+		else{}
+			?>
+	
+	<h1> Ecriture d'un chapitre </h1>
+
+	<form action="" method="post">
+		<div>
+			<label class="col-form-label" for="title">Titre</label><br />
+			<input class="form-control" type="text" id="title" name="title" required />
+		</div> <br/>
+
+		<div>
+			<label class="col-form-label" for="chapter">Chapitre n°</label>
+			<input class="form-control" type="number" id="chapter" name="chapter" required />
+		</div> <br/>
+
+		<div>
+			<label class="col-form-label" for="content">Texte</label><br />
+			<textarea class="form-control" id="content" name="content"></textarea>
+		</div> <br/>
+		<div>
+			<input class="btn" name="add" type="submit" />
+		</div>
+	</form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../view/template.php');?>
+
+<?php require('../view/chapter.php');?>
