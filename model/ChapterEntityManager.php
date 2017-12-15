801fd134ee10c482cb9eabac4e629eb1148f0844
arthur32810 <arthur32810@hotmail.fr>
2017-12-15 12:39:49 +0100

    Ajout de la fonction création de chapitre

--- model/ChapterEntityManager.php
+++ model/ChapterEntityManager.php
@@ -8,12 +8,23 @@
 			return $req;
 		}
 
-		public static function getChapter($post){
+		public static function getChapter($chapter){
 			$db = DBManager::dbConnect();
 			$req = $db->prepare('SELECT id, chapter, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') 
 								 AS creation_date_fr FROM chapter WHERE id = ? OR chapter = ?');
-			$req->execute(array($post->getId(), $post->getChapter()));
-			$post = $req->fetch();
-			return $post;
+			$req->execute(array($chapter->getId(), $chapter->getChapter()));
+			$chapter = $req->fetch();
+			return $chapter;
+		}
+
+		public function createChapter($chapter){
+			$db = Manager::dbConnect();
+
+			$addPost = $db->prepare('INSERT INTO chapter (chapter, title, content, creation_date) VALUES (:chapter, :title, :content, NOW())');
+			$addPost->execute(array(
+							'chapter' => $chapter->getChapter(),
+							'title' => $chapter->getTitle(),
+							'content' => $chapter->getContent()));
+			return $addPost;
 		}
 	}
